a5a6e8f0333eafafe01716a692ca00d2d2f03707
JoseArroyave <jarroyave@unal.edu.cogit config --global user.email jarroyave@unal.edu.co>
2022-03-29 01:18:33 -0500

    new db connection in jwasdb-heroku

--- includes/functions/bd_conexion.php
+++ includes/functions/bd_conexion.php
@@ -1,5 +1,10 @@
 <?php
-$conn = new mysqli('kutnpvrhom7lki7u.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 've3js2zqtjrenuz5', 'xvrds8r2bgiola4f', 'ugg7505nzucko9hm');
+// $conn = new mysqli(URL, USERNAME, PASSWORD, 'DATABASE ID');
+$url = 'kutnpvrhom7lki7u.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+$username = 'x3gibc7zmsfresxr';
+$password = 'i3m6nobc78t2rtyo';
+$database = 'gqdytjbo00dyuv2j';
+$conn = new mysqli($url, $username, $password, $database);
 if (!$conn->set_charset("utf8")) {
   $conn->set_charset("utf8");
 } else {
