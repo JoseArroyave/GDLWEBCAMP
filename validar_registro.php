050be75207628fd5d4e84b2ab4f0fce75e41942c
JoseArroyave <jarroyave@unal.edu.cogit config --global user.email jarroyave@unal.edu.co>
2022-03-29 01:51:29 -0500

    prueba 5

--- validar_registro.php
+++ validar_registro.php
@@ -43,10 +43,10 @@ endif;
 ?>
 <?php include_once 'includes/templates/header.php'; ?>
 <section class="seccion contenedor" id="reservas">
-  <?php if (isset($_GET['exitoso'])) :
-    if ($_GET['exitoso'] == "1") :
-      echo '<h2>' . 'Resumen registro' . '</h2>';
-    endif;
-  endif; ?>
+  <?php if (isset($_GET['exitoso'])) {
+    if ($_GET['exitoso'] == "1") { ?>
+      <h2>Registro exitoso</h2>
+  <?php }
+  } ?>
 </section>
 <?php include_once 'includes/templates/footer.php'; ?>
