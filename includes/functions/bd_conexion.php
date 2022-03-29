cd17f0887b613a705d18dbeb7a23046bc1273163
JoseArroyave <jarroyave@unal.edu.cogit config --global user.email jarroyave@unal.edu.co>
2022-03-29 01:00:55 -0500

    new db connection in jwasdb-heroku

--- includes/functions/bd_conexion.php
+++ includes/functions/bd_conexion.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli('localhost', 'root', '', 'gdlwebcamp');
+$conn = new mysqli('kutnpvrhom7lki7u.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 've3js2zqtjrenuz5', 'xvrds8r2bgiola4f', 'ugg7505nzucko9hm');
 if (!$conn->set_charset("utf8")) {
   $conn->set_charset("utf8");
 } else {
